b02136e6e8a87e135172834362d63e81dbf43aab
marisaGcarneiro <92409306+marisaGcarneiro@users.noreply.github.com>
2024-03-12 20:34:47 -0300

    sistema-smart-tv
    Desenvolvendo um sistema de interagir com estatistica para smart tv.

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,13 @@
+public class SmartTv {
+    
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    puclic void ligar(){
+        ligada = true;
+    }
+    puclic void desligar(){
+        ligada = false;
+    }
+}
